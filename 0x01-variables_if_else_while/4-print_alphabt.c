c2597d9191ae6c73c4b68ba9338130e6c5e6f3b8
ealpha072 <ealpha072@gmail.com>
2022-10-15 22:01:31 +0000

    Corrections again

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -14,9 +14,9 @@ int main(void)
 
 	for (low = 'a'; low <= 'z'; low++)
 	{
-		if (low == "e" || low == "q")
+		if (low = "e" || low = "q")
 			continue;
-		puthchar(low);
+		putchar(low);
 	}
 	putchar('\n');
 
