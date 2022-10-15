7abc515f4ed2b387a3c42b18e396ddcdf3c7316d
ealpha072 <ealpha072@gmail.com>
2022-10-15 18:29:46 +0000

    Extra task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry Point
+ * Return: Always 1 (Success)
+ */
+
+int main(void)
+{
+	write(STDOUT_FILE, "and that piece of art is usefull
+			\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
