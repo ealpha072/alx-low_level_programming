2ab9c9ff3cf8449cfc9ec2d5123998bf18466c51
ealpha072 <ealpha072@gmail.com>
2022-10-18 19:39:16 +0000

    Correction three and four

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,13 +10,13 @@ int print_alphabet_x10(void)
 {
 	char letter;
 
-	for (int i = '0'; i <= '9'; i++)
+	for (int i = 0; i <= 9; i++)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 		{
 			_putchar(letter);
 		}
-	_putchar('\n');
+		_putchar('\n');
 	}
 }
 
