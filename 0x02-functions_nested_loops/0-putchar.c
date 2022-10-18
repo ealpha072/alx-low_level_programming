681e311d74edee61b7160e0777da70ed3efaaf57
ealpha072 <ealpha072@gmail.com>
2022-10-18 20:50:54 +0300

    Folders added for initial files

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "_putchar.c"
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints _putchar followed by new line
+ * Return: 0 
+ */
+
+int main(void)
+{
+	_putchar.c("_putchar");
+	return (0);
+}
