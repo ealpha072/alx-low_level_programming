39ff61f49a7752c7af2607344b864aa2fc6da263
ealpha072 <ealpha072@gmail.com>
2022-10-18 19:10:45 +0000

    Fixed betty issues

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,19 @@
 #include "main.h"
 
 /**
- * main: Entry Point
- * 
+ * print_alphabet - function to print abc
+ *
  * Return: Always 0: Success
 */
 
-void print_alphabet(void){
-    char letter;
-    for ( letter = 'a'; letter <= 'z'; letter++)
-    {
-        _putchar(letter);
-    }
-    _putchar('\n');
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+
+	_putchar('\n');
 }
