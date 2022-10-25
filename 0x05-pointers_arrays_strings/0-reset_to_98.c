0981a0f3ac7d11a4190febe62e68cbf327b1ed6d
ealpha072 <ealpha072@gmail.com>
2022-10-25 17:52:15 +0000

    Arrays and pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes pointer and updates value to 98
+ * @n: pointer to convert
+ * Return: Void on success
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+	printf("%x", *n);
+}
