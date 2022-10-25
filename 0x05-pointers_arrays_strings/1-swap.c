d7779261e166a92c5db87f37170990d817f785e7
ealpha072 <ealpha072@gmail.com>
2022-10-25 17:59:49 +0000

    Task two

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps values of two integeres
+ * @a:first int
+ * @b:first int
+ *
+ * Return: void on success
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = *a;
+}
